2dc03e57a0ea1de903d217663e8fb6472e9c4911
Koenig Juan <koenigjuan@gmail.com>
2023-12-11 20:20:58 -0300

    primera entrega y correccion inmediata

--- solution.js
+++ solution.js
@@ -1,3 +1,5 @@
+
+
 let first_list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let second_list = [6, 4, 3, 2, 7, 9, 11, 12];
 let third_list = [12, 4, 3, 2, 7, 9, 11, 6];
@@ -6,14 +8,14 @@ function solution(num_list) {
     var solution = num_list[0];
     for (i = 0; i < num_list.length; i++) {
         if (num_list[i] > solution ) {
-            var solution = num_list[i + 1]
+            var solution = num_list[i]
         } 
     }
 
     return solution
 }
 
-console.log(solution(second_list))
+console.log(solution(first_list))
 
 
 
